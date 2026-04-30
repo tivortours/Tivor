948c3e2a6fb0dc6c737abeaaa4fcea4a8ae98ec2
mantra-info <albie@mantraitsolutions.in>
2026-04-30 14:43:42 +0530

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default async function HomePage() {
     <main className="flex w-full flex-col overflow-x-hidden">
       <section className="relative min-h-screen overflow-hidden bg-[#c0bebe]">
         <Image src={home.heroImage} alt="" fill priority className="object-cover" sizes="100vw" />
-        <div className="absolute inset-0 bg-black/20" />
+        {/* <div className="absolute inset-0 bg-black/20" /> */}
         <div
           className="pointer-events-none absolute inset-x-0 bottom-0 bg-gradient-to-b from-transparent to-black/70"
           style={{ height: "41.3%" }}
